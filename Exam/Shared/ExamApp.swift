a0982c653c7428471361d731ffe0503dd4ef6fc6
daniilpazin <pazin.d@icloud.com>
2022-02-03 21:01:47 +0300

    Project created.

---
+++ Exam/Shared/ExamApp.swift
@@ -0,0 +1,20 @@
+//
+//  ExamApp.swift
+//  Shared
+//
+//  Created by Пазин Даниил on 03.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExamApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
